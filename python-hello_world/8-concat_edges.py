f3c43220141168c0619de051aec4a8eb778ba46a
antonin-crypto <akamis53@gmail.com>
2024-05-06 15:51:08 +0200

    modified 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,5 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(''.join(str.split()[5:6]  +  [' programming with Python']))
+str = str[39:66]+" "+str[107:111]+" "+str[0:6]
+print(str)
