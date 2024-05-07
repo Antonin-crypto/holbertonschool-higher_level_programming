1bae259bfc841a0522e79e07b918efeb87f87551
antonin-crypto <akamis53@gmail.com>
2024-05-07 10:53:09 +0200

    modified 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 def uppercase(str):
+    result = ""
     for char in str:
         if 97 <= ord(char) <= 122:
-            print("{}".format(chr(ord(char) - 32)), end='')
+            result += chr(ord(char) - 32)
         else:
-            print("{}".format(char), end='')
-    print("")
+            result += char
+    result += "\n"
+    print(result, end="")
     return
