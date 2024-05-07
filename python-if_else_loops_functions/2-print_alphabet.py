7799742ce9d2e4af3302992e30abe6be107aabda
antonin-crypto <akamis53@gmail.com>
2024-05-07 09:42:42 +0200

    modified file 2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-ascii_value = ord('a')
 
-while ascii_value <= ord('z'):
-    print(chr(ascii_value), end='')
-    ascii_value += 1
+
+for i in range(97, 123):
+    print("{}".format(chr(i)), end='')
