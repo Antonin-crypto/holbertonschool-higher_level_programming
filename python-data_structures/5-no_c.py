814e50ef0b519af3d31d7a410a56383572689d70
antonin-crypto <akamis53@gmail.com>
2024-05-13 12:57:30 +0200

    test 5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    return my_string.replace('c', '').replace('C', '')
+    new_string = ''
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
