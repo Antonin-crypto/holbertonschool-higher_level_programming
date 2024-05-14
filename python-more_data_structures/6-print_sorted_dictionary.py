7d8751357e29c2e1dd1ecdbd33c67fa4e0d06cd2
antonin-crypto <akamis53@gmail.com>
2024-05-14 09:01:03 +0200

    modified 6-print non espace {} :

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 
 def print_sorted_dictionary(a_dictionary):
     for index in sorted(a_dictionary):
-        print("{} : {}".format(index, a_dictionary[index]))
+        print("{}: {}".format(index, a_dictionary[index]))
