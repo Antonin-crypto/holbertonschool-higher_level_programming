8128a550eb751ece22e6e819538fcf1151807a5a
antonin-crypto <akamis53@gmail.com>
2024-05-14 09:30:39 +0200

    modified file 9  for pycodestyle

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -3,6 +3,5 @@
 def multiply_by_2(a_dictionary):
     multiply_copy = {}
     for key, value in a_dictionary.items():
-        multiply_copy[key]= value * 2
+        multiply_copy[key] = value * 2
     return multiply_copy
-
