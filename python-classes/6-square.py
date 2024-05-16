29d9f3bfd18390a1aff2d849d528e4c11f53bfeb
antonin-crypto <akamis53@gmail.com>
2024-05-16 14:50:23 +0200

    test modified file 6-square print and return

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -7,31 +7,31 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """Initialise un carré avec une taille optionnelle."""
-        self.size = size  # Définit la taille du carré
+        self.size = size
         self.position = position
 
     @property
     def size(self):
         """Obtient la taille du carré."""
-        return self._size  # Retourne la taille du carré
+        return self._size
 
     @size.setter
     def size(self, value):
         """Définit la taille du carré avec des vérifications."""
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")  # Vérifie si la taille
+            raise TypeError("size must be an integer")
         if value < 0:
-            raise ValueError("size must be >= 0")  # Vérifie si la taille negat
-        self._size = value  # Définit la taille du carré
+            raise ValueError("size must be >= 0")
+        self._size = value
 
     @property
     def position(self):
-        """definir la position actuelle"""
+        """obtient la position du carre"""
         return self._position
 
     @position.setter
     def position(self, value):
-        """definit la position."""
+        """Définit la position du carré avec des vérifications."""
         if (not isinstance(value, tuple) or len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
@@ -40,15 +40,14 @@ class Square:
 
     def area(self):
         """Calcule et retourne l'aire du carré."""
-        return (self._size * self._size)  # Retourne l'aire du carré
+        return (self.__size * self.__size)
 
     def my_print(self):
         """imprimer la valeur de la taille"""
         if self._size == 0:
             print()
-
+            return
         for _ in range(self._position[1]):
             print()
-
         for _ in range(self._size):
             print(" " * self._position[0] + "#" * self._size)
