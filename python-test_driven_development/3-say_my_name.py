4414cb2251c244d628a5d9caf0ddb9df80971564
antonin-crypto <akamis53@gmail.com>
2024-05-20 15:46:48 +0200

    pycodestyle for 3-say_name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """prints My name is <first name> <last name>"""
 
+
 def say_my_name(first_name, last_name=""):
     """prints My name is <first name> <last name>"""
     """Type first_name must be a string or last_name must be a string"""
