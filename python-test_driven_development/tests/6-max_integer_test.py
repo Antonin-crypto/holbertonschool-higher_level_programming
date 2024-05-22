159bc4ac9e20a256b03447e83ee48e6e83706048
antonin-crypto <akamis53@gmail.com>
2024-05-22 17:28:52 +0200

    test 6

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -24,3 +24,10 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_duplicate_max(self):
         self.assertEqual(max_integer([10, 20, 30, 30]), 30)
+
+    def test_unmodified_list(self):
+        test_list = [873, 5654, 465, -389, 0]
+        test_copy = list(test_list)
+        self.assertTrue(len(test_copy) == len(test_list))
+        for i in range(len(test_copy)):
+            self.assertTrue(test_copy[i] == test_list[i])
