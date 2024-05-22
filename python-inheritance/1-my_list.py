14a874bf3d436ecb1afae1542e7b0253239ae182
antonin-crypto <akamis53@gmail.com>
2024-05-22 09:45:32 +0200

    oublier de You can assume that all the elements of the list will be of type int for task 1 and test int

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,5 +6,8 @@ class MyList(list):
     """Custom list class"""
     def print_sorted(self):
         """Prints the elements of the list in sorted order"""
+        for element in self:
+            if not isinstance(element, int):
+                raise TypeError("not all the elements type int")
         sorted_list = sorted(self)
         print(sorted_list)
