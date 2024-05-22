9544bf0d92d47b8bb1a18c298d0d499bff3a0297
antonin-crypto <akamis53@gmail.com>
2024-05-22 17:43:06 +0200

    test 6

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -7,27 +7,40 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    def test_empty_list(self):
-        self.assertIsNone(max_integer([]))
+    """Define unittests for max_integer function."""
+
+    def test_ordered_list(self):
+        """Test with a list of ordered integers."""
+        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
+
+    def test_unordered_list(self):
+        """Test with a list of unordered integers."""
+        self.assertEqual(max_integer([1, 3, 4, 2, 5]), 5)
+
+    def test_all_negative(self):
+        """Test with a list of all negative numbers."""
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+
+    def test_negative_and_positive(self):
+        """Test with a list containing both positive and negative integers."""
+        self.assertEqual(max_integer([-1, 1, -2, 2, 0]), 2)
 
     def test_single_element(self):
-        self.assertEqual(max_integer([42]), 42)
+        """Test with a list containing a single element."""
+        self.assertEqual(max_integer([7]), 7)
 
-    def test_positive_numbers(self):
-        self.assertEqual(max_integer([10, 20, 30]), 30)
+    def test_empty_list(self):
+        """Test with an empty list."""
+        self.assertIsNone(max_integer([]))
 
-    def test_negative_numbers(self):
-        self.assertEqual(max_integer([-5, -10, -2]), -2)
+    def test_floats(self):
+        """Test with a list of floats."""
+        self.assertEqual(max_integer([1.5, 2.8, 3.1, 0.5]), 3.1)
 
-    def test_mixed_numbers(self):
-        self.assertEqual(max_integer([5, -8, 12, 0]), 12)
+    def test_mixed_types(self):
+        """Test with a list containing integers and floats."""
+        self.assertEqual(max_integer([1, 2.5, 3, 4.4]), 4.4)
 
-    def test_duplicate_max(self):
-        self.assertEqual(max_integer([10, 20, 30, 30]), 30)
-
-    def test_unmodified_list(self):
-        test_list = [873, 5654, 465, -389, 0]
-        test_copy = list(test_list)
-        self.assertTrue(len(test_copy) == len(test_list))
-        for i in range(len(test_copy)):
-            self.assertTrue(test_copy[i] == test_list[i])
+    def test_identical_elements(self):
+        """Test with a list containing identical elements."""
+        self.assertEqual(max_integer([2, 2, 2, 2]), 2)
