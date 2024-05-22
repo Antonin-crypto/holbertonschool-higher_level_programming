32472a2fa6dcfaaacc5275393e026fd544e67a6a
antonin-crypto <akamis53@gmail.com>
2024-05-22 08:40:37 +0200

    cree all file for Python - Inheritance and task 0. Lookup

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""function that returns the list of available attributes"""
+
+def lookup(obj):
+    """Returns the list of available attributes for the given object.
+
+    Args:
+        obj: The object to inspect.
+
+    Returns:
+        A list of strings representing the available attributes of the object.
+    """
+    return dir(obj)
