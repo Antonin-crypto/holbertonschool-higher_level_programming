9855d343b1d32100875b3a53617f3628aaeeab6f
antonin-crypto <akamis53@gmail.com>
2024-05-27 09:58:56 +0200

    cree all file 0-read_fied and directory python-input_output

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""contains the file Read"""
+
+
+def read_file(filename=""):
+    """function that reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end='')
