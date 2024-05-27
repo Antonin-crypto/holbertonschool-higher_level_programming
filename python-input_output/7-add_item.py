b572a7f97c615764eb38ed844d55c8bc2de3c394
antonin-crypto <akamis53@gmail.com>
2024-05-27 13:22:24 +0200

    checker ?

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Adds all arguments to a Python list and saves them to a file."""
+"""script for adding item in json list"""
 
 
 import sys
@@ -7,11 +7,13 @@ import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    json_list = load_from_json_file('add_item.json')
-except FileNotFoundError:
-    json_list = []
 
-for i in range(1, len(sys.argv)):
-    json_list.append(sys.argv[i])
-save_to_json_file(json_list, "add_item.json")
+if __name__ == "__main__":
+    try:
+        json_list = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        json_list = []
+
+    for i in range(1, len(sys.argv)):
+        json_list.append(sys.argv[i])
+    save_to_json_file(json_list, "add_item.json")
