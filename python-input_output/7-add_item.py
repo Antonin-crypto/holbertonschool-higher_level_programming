8e96b168d46af301e2b67a202136678d8b5b4076
antonin-crypto <akamis53@gmail.com>
2024-05-27 12:58:20 +0200

    test modified and try

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,7 +4,6 @@
 
 import json
 import sys
-from os import path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
@@ -13,18 +12,15 @@ def add_item_to_list_and_save():
     """script that adds all arguments to a Python list,
     and then save them to a file:"""
     # Check if the file exists
-    if path.exists("add_item.json"):
-        # Load existing data
-        data = load_from_json_file("add_item.json")
-    else:
-        data = []
+    try:
+        json_list = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        json_list = []
 
-    # Add arguments to the list
-    for arg in sys.argv[1:]:
-        data.append(arg)
-
+    for arg in range(1, len(sys, argv)):
+        json_list.append(sys.argv[arg])
     # Save the list to a JSON file
-    save_to_json_file(data, "add_item.json")
+    save_to_json_file(json_list, "add_item.json")
 
 
 if __name__ == "__main__":
