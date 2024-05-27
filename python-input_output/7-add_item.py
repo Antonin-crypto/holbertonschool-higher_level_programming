0d4f3a8ede46448170fcf728882332ad92c9acdb
antonin-crypto <akamis53@gmail.com>
2024-05-27 13:11:25 +0200

    test

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,31 +1,18 @@
 #!/usr/bin/python3
-"""
-Adds all arguments to a Python list and saves them to a file.
-"""
+"""Adds all arguments to a Python list and saves them to a file."""
 
 
 import sys
 import json
-from os import path
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
+try:
+    json_list = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    json_list = []
 
-def add_item():
-    """Adds all arguments to a Python list and saves them to a file."""
-    filename = 'add_item.json'
-    items = []
+for i in range(1, len(sys.argv)):
+    json_list.append(sys.argv[i])
 
-    # Check if the file already exists and load its content
-    if path.exists(filename):
-        items = load_from_json_file(filename)
-
-    # Add all command line arguments to the list
-    items.extend(sys.argv[1:])
-
-    # Save the list to a JSON file
-    save_to_json_file(items, filename)
-
-
-if __name__ == "__main__":
-    add_item()
+save_to_json_file(json_list, "add_item.json")
