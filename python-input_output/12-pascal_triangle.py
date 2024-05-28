b2c308d6c6715c8cb993d70ab68c8444992a1b71
antonin-crypto <akamis53@gmail.com>
2024-05-28 11:09:53 +0200

    test

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""contais the Pascal's Triangle"""
+"""create a pascal triangle"""
 
 
 def pascal_triangle(n):
-    """returns a list of lists of integers representing
-    the Pascals triangle of n"""
+    """ create a pascal triangle """
     if n <= 0:
-        return[]
+        return []
 
     triangle = [[1]]
     for i in range(1, n):
         row = [1]
-        prev_row = triangle[i-1]
+        prev_row = triangle[i - 1]
 
         for j in range(1, i):
-            row.append(prev_row[j-1] + prev_row[j])
+            row.append(prev_row[j - 1] + prev_row[j])
         row.append(1)
 
         triangle.append(row)
