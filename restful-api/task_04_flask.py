db1bd6cc44c2612133cd06df4a361a054e72d3de
antonin-crypto <akamis53@gmail.com>
2024-05-30 14:58:47 +0200

    test commentaire for task 4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""contains the Develop a Simple API using Python with Flask"""
-
+"""Développer une API simple en utilisant Python avec Flask"""
 
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+
+# Dictionnaire des utilisateurs
 users = {
     "jane": {"name": "Jane", "age": 28, "city": "Los Angeles"},
     "john": {"name": "John", "age": 30, "city": "New York"}
@@ -14,35 +15,35 @@ users = {
 
 @app.route('/')
 def home():
-    """contains the home"""
-    return "Welcome to the Flask API!"
+    """Route de la page d'accueil"""
+    return "Bienvenue dans l'API Flask !"
 
 
 @app.route('/data')
 def get_data():
-    """contais the data"""
+    """Route pour obtenir tous les noms d'utilisateur"""
     return jsonify(list(users.keys()))
 
 
 @app.route('/status')
 def status():
-    """contais the status"""
+    """Route de statut"""
     return "OK"
 
 
 @app.route('/users/<username>')
 def get_user(username):
-    """contais the get_user"""
+    """Route pour obtenir les informations d'un utilis par nom d'utilisateur"""
     user = users.get(username)
     if user:
         return jsonify(user)
     else:
-        return jsonify({"error": "User not found"}), 404
+        return jsonify({"error": "Utilisateur non trouvé"}), 404
 
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
-    """contains the add_user"""
+    """Route pour ajouter un nouvel utilisateur"""
     user_data = request.json
     username = user_data.get('username')
     if username and username not in users:
@@ -51,11 +52,11 @@ def add_user():
             "age": user_data.get('age'),
             "city": user_data.get('city')
         }
-        return jsonify({"message": "User added", "user": users[username]})
+        return jsonify({"message": "Utilisat ajouté", "user": users[username]})
     else:
-        return jsonify({"error": "Invalid data or user already exists"}), 400
+        return jsonify({"error": "Données invalides ou utilisa exi déjà"}), 400
 
 
-"""contais the name"""
 if __name__ == '__main__':
+    # Démarrer le serveur de développement Flask
     app.run(debug=True)
