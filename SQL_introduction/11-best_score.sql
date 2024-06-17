d61ccb334b7f7206fd6c6d39f2a12507d9c21c3a
Antonin-crypto <akamis53@gmail.com>
2024-06-17 14:58:08 +0200

    score non maj

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- List the score greather than or equal to 10
-SELECT SCORE,name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score,name FROM second_table WHERE score >= 10 ORDER BY score DESC;
