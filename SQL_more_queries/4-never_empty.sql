62377fff87cccf5e0f06681e0a8e19609b99de0b
Antonin-crypto <akamis53@gmail.com>
2024-06-18 14:43:26 +0200

    defaut forget the l

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- create a table with defaut 1
-CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAUT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
