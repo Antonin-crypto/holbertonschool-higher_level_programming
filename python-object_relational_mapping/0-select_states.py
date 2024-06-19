8906e0090695912f568887c1675dbb5286034286
Antonin-crypto <akamis53@gmail.com>
2024-06-19 10:52:27 +0200

    non argument a password

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,16 +2,16 @@
 """Module listing all states from the database"""
 
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
     # Connect to the database
     db = MySQLdb.connect(
           host="localhost",
           port=3306,
-          user=argv[1],
-          passwd=argv[2],
-          db=argv[3],
+          user=sys.argv[1],
+          passwd="",
+          db=sys.argv[3],
           )
 
     # Create a cursor object to interact with the database
