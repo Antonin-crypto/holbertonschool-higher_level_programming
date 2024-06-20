faf75cb29aa5b280562ece830146447e1728e879
Antonin-crypto <akamis53@gmail.com>
2024-06-20 13:16:42 +0200

    modified 5 INNER JOIN cities ON states.id = cities.state_id\         WHERE states.name = % s\         ORDER BY cities.id""",

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,33 +1,30 @@
 #!/usr/bin/python3
-"""Module listing all cities of a state from the database hbtn_0e_4_usa"""
+"""Lists all states with a name starting with N (upper N)"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    # Connect to the database
-    db = MySQLdb.connect(host="localhost",
-                         user=argv[1],
-                         passwd=argv[2],
-                         port=argv[3])
 
-    # Create a cursor object to interact with the database
+    db = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        port=3306
+    )
+
+    cities = []
     cur = db.cursor()
+    if len(argv) == 5:
+        cur.execute("""SELECT cities.name FROM states\
+        INNER JOIN cities ON states.id = cities.state_id\
+        WHERE states.name = % s\
+        ORDER BY cities.id""", (argv[4],))
 
-    # Execute the SQL query with parameterized input to prevent SQL injection
-    cur.execute("""SELECT name
-                FROM cities
-                 WHERE cities.state_id = (SELECT id
-                    FROM states
-                    WHERE name = %(state)s)
-                ORDER BY cities.id ASC""", {argv[4]})
-
-    # Fetch all the rows returned by the query
-    query_rows = cur.fetchall()
-
-    # Print each row
-    city_names = ', '.join([row[0] for row in query_rows])
-    print(city_names)
-    # Close the cursor and connection
+        rows = cur.fetchall()
+        for row in rows:
+            cities.append(row[0])
+    print("{}".format(', '.join(cities)))
     cur.close()
     db.close()
