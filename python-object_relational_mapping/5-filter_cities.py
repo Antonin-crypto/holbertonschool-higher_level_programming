fd98df15b9276aee3fac12eff8a123ed99b5c96c
Antonin-crypto <akamis53@gmail.com>
2024-06-20 09:54:46 +0200

    syntaxe

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -6,11 +6,10 @@ from sys import argv
 
 if __name__ == "__main__":
     # Connect to the database
-    db = MySQLdb.connect("localhost",argv[1],argv[2], argv[3])
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
 
     # Create a cursor object to interact with the database
     cur = db.cursor()
-
 
     # Execute the SQL query with parameterized input to prevent SQL injection
     cur.execute("""SELECT name
@@ -18,8 +17,7 @@ if __name__ == "__main__":
                  WHERE cities.state_id = (SELECT id
                     FROM states
                     WHERE name = %(state)s)
-                ORDER BY cities.id ASC""",
-        {"state": argv[4]})
+                ORDER BY cities.id ASC""", {"state": argv[4]})
 
     # Fetch all the rows returned by the query
     query_rows = list(cur.fetchall())
