77b3bfeccd9e2d79454b3d331119ffb467197eca
Antonin-crypto <akamis53@gmail.com>
2024-06-20 11:12:50 +0200

    modified print and host file 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -6,7 +6,10 @@ from sys import argv
 
 if __name__ == "__main__":
     # Connect to the database
-    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         passwd=argv[2],
+                         port=argv[3])
 
     # Create a cursor object to interact with the database
     cur = db.cursor()
@@ -20,12 +23,11 @@ if __name__ == "__main__":
                 ORDER BY cities.id ASC""", {"state": argv[4]})
 
     # Fetch all the rows returned by the query
-    query_rows = list(cur.fetchall())
+    query_rows = cur.fetchall()
 
     # Print each row
-    for row in range(len(query_rows)):
-        query_rows[row] = str(query_rows[row][0])
-        print(', '.join(query_rows))
+    city_names = ', '.join([row[0] for row in query_rows])
+    print(city_names)
     # Close the cursor and connection
     cur.close()
     db.close()
